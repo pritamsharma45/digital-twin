7c8e28b51c8ce36817e2b0bf649925905ab76071
pritam sharma <notifications.borl@gmail.com>
2024-10-04 14:28:02 +0530

    sddfasda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
 // const socket = io('http://localhost:3001');
-const socket = io('https://energy-meter-backend.onrender.com');
+const socket = io("https://energy-meter-backend.onrender.com");
 
 function EnergyMeter() {
   const [reading, setReading] = useState(1000);
 
   useEffect(() => {
-    socket.on('newReading', (newReading) => {
+    socket.on("newReading", (newReading) => {
       setReading(newReading);
     });
 
-    return () => socket.off('newReading');
+    return () => socket.off("newReading");
   }, []);
 
   return (
     <div>
       <h1>Live Energy Meter</h1>
       <p>Current Reading: {reading} kWh</p>
+      <p>Status</p>
     </div>
   );
 }
